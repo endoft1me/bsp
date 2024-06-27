97c3df94028369f8a613be875c4f0417058ae55e
highpn <marcibarat@gmail.com>
2024-06-27 21:30:13 +0200

    dasfsd

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+int matrix[50][50];
 int main(){
     printf("sadf");
+    for(int x=0;x<50;x++)
+    for(int y=0;y<50;y++)
+    matrix[x][y]='.';
+    for(int x=0;x<50;x++)
+    for(int y=0;y<50;y++)
+    {
+    matrix[x][y]='.';
+    }
+    printf("\n");
     return 0;
 }
